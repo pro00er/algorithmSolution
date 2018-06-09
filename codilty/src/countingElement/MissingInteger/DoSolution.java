d48daa11f814965826eb80a2f04d7e7294044752
ohahohah <ohahohah.dev@gmail.com>
2018-06-09 10:42:19 +0900

    Upload Solution Codility Lesson04

---
+++ codilty/src/countingElement/MissingInteger/DoSolution.java
@@ -0,0 +1,12 @@
+package codility;
+
+public class DoSolution {
+
+  private static final int DUMMY_VALUE = -1;
+
+  public static void main(String[] args){
+    Solution sol = new Solution();
+    //Only for run solution - DUMMY_VALUE doesn't affect anything
+    int result = sol.solution(DUMMY_VALUE);
+  }
+}
